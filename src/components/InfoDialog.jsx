80f5485da3222e1903a21935100b20374ed9745c
Anthony Wang <52114040+anthonyftwang@users.noreply.github.com>
2022-01-09 23:34:46 -0500

    feat: multi-purpose info dialog

---
+++ src/components/InfoDialog.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@mui/material';
+
+/**
+ * Simple dialog that displays modal text and can then be dismissed.
+ */
+
+function InfoDialog({ title, text, open, onClose }) {
+  const closeDialog = () => {
+    onClose();
+  };
+
+  return (
+    <Dialog open={open} onClose={closeDialog}>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText sx={{ whiteSpace: 'pre-line' }}>
+          {text}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={closeDialog}>Close</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+InfoDialog.propTypes = {
+  /** The title of the dialog. */
+  title: PropTypes.string.isRequired,
+  /** Further information that the dialog conveys to the user. */
+  text: PropTypes.string.isRequired,
+  /** Determines whether dialog is visible. */
+  open: PropTypes.bool.isRequired,
+  /** Callback when dialog is closed. */
+  onClose: PropTypes.func.isRequired,
+};
+
+export default InfoDialog;
