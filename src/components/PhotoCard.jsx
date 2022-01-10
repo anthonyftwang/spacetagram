a19370f01273844207503799c63e08528ec712de
Anthony Wang <52114040+anthonyftwang@users.noreply.github.com>
2022-01-09 23:39:48 -0500

    feat: display photos in list of cards

---
+++ src/components/PhotoCard.jsx
@@ -0,0 +1,171 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import requiredIf from 'react-required-if';
+import moment from 'moment';
+import {
+  IconButton,
+  Chip,
+  Card,
+  CardContent,
+  CardMedia,
+  CardActions,
+  Link,
+  Stack,
+  Typography,
+} from '@mui/material';
+import CopyrightIcon from '@mui/icons-material/Copyright';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import useStateWithLocalStorage from 'hooks/useStateWithLocalStorage';
+import localization from 'util/strings';
+
+/**
+ * Card that displays all info related to a photo.
+ */
+
+function PhotoCard({
+  date,
+  title,
+  explanation,
+  copyright,
+  url,
+  hdurl,
+  mediaType,
+}) {
+  const [showAll, setShowAll] = useState(false);
+  const [liked, setLiked] = useStateWithLocalStorage(date);
+
+  const truncate = (text, fuzzyCharLength) => {
+    if (text.length <= fuzzyCharLength) {
+      return text;
+    }
+    const strict = text.substr(0, fuzzyCharLength - 1);
+    return (
+      <>
+        {strict.substr(0, strict.lastIndexOf(' '))}
+        &hellip;
+      </>
+    );
+  };
+
+  const openInHD = () => {
+    window.open(hdurl, '_blank').focus();
+  };
+
+  return (
+    <Card
+      sx={{ minWidth: 275, mb: 'var(--spacetagram-main-padding)' }}
+      variant="outlined"
+    >
+      {mediaType === 'image' ? (
+        <CardMedia
+          component="img"
+          image={url}
+          alt={`${localization.imgAltTemplate} ${moment(date).format(
+            'MMM D, YYYY'
+          )}`}
+        />
+      ) : (
+        <CardMedia
+          component="iframe"
+          src={url}
+          title={`${localization.imgAltTemplate} ${moment(date).format(
+            'MMM D, YYYY'
+          )}`}
+        />
+      )}
+      <CardContent>
+        <Typography
+          gutterBottom
+          variant="body2"
+          color="text.secondary"
+          component="div"
+        >
+          {` ${moment(date).format('MMM D, YYYY')}`}
+        </Typography>
+        <Typography gutterBottom variant="h5">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {showAll ? (
+            <>
+              {explanation}
+              {/* Use of achor as button is idiomatic for "Read more/less" */}
+              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+              <Link
+                component="button"
+                color="inherit"
+                sx={{ display: 'block', mt: 'var(--spacetagram-text-padding)' }}
+                onClick={() => setShowAll(false)}
+              >
+                Read less
+              </Link>
+            </>
+          ) : (
+            <>
+              {truncate(explanation, 150)}
+              {/* Use of achor as button is idiomatic for "Read more/less" */}
+              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+              <Link
+                component="button"
+                color="inherit"
+                sx={{ display: 'block', mt: 'var(--spacetagram-text-padding)' }}
+                onClick={() => setShowAll(true)}
+              >
+                Read more
+              </Link>
+            </>
+          )}
+        </Typography>
+        <Stack
+          direction="row"
+          spacing={1}
+          sx={{ mt: 'var(--spacetagram-main-padding)' }}
+        >
+          {copyright && (
+            <Chip
+              icon={<CopyrightIcon />}
+              label={copyright}
+              variant="outlined"
+            />
+          )}
+        </Stack>
+      </CardContent>
+      <CardActions>
+        <Stack direction="row" spacing={1}>
+          <IconButton onClick={() => setLiked(!liked)}>
+            {liked ? <FavoriteIcon color="primary" /> : <FavoriteBorderIcon />}
+          </IconButton>
+          <IconButton onClick={openInHD} disabled={!hdurl}>
+            <OpenInNewIcon />
+          </IconButton>
+        </Stack>
+      </CardActions>
+    </Card>
+  );
+}
+
+PhotoCard.propTypes = {
+  /** The unique date corresponding to the photo. */
+  date: PropTypes.string.isRequired,
+  /** The title of the photo. */
+  title: PropTypes.string.isRequired,
+  /** The written explanation accompanying the photo. */
+  explanation: PropTypes.string.isRequired,
+  /** The owner(s) of the copyrights to the media. */
+  copyright: PropTypes.string,
+  /** The url of the photo or video source. */
+  url: PropTypes.string.isRequired,
+  /** The location of the full-resolution source media. */
+  hdurl: requiredIf(PropTypes.string, (props) => props.mediaType === 'image'),
+  /** The type of media to be displayed. */
+  mediaType: PropTypes.oneOf(['image', 'video']).isRequired,
+};
+
+PhotoCard.defaultProps = {
+  copyright: '',
+  hdurl: '',
+};
+
+export default PhotoCard;
---
+++ src/views/PhotoList.jsx
@@ -0,0 +1,145 @@
+import React, { useState, useEffect } from 'react';
+import {
+  IconButton,
+  FormControl,
+  Select,
+  MenuItem,
+  Typography,
+  Stack,
+  Pagination,
+} from '@mui/material';
+import InfoIconOutlined from '@mui/icons-material/InfoOutlined';
+import getPhotosService from 'services/getPhotos.service';
+import PhotoCard from 'components/PhotoCard';
+import InfoDialog from 'components/InfoDialog';
+import Loading from 'components/Loading';
+import localization from 'util/strings';
+
+/**
+ * Composite component that houses scrolling photo view.
+ */
+
+function PhotoList() {
+  const [photos, setPhotos] = useState([]);
+  const [pageParams, setPageParams] = useState({ page: 1, pageLength: 5 });
+  const [infoDialogVisible, setInfoDialogVisible] = useState(false);
+  const [errorDialogVisible, setErrorDialogVisible] = useState(false);
+
+  const showInfoDialog = () => {
+    setInfoDialogVisible(true);
+  };
+
+  const hideInfoDialog = () => {
+    setInfoDialogVisible(false);
+  };
+
+  const showErrorDialog = () => {
+    setErrorDialogVisible(true);
+  };
+
+  const hideErrorDialog = () => {
+    setErrorDialogVisible(false);
+  };
+
+  const fetchPhotos = async () => {
+    try {
+      const resp = await getPhotosService(
+        pageParams.page,
+        pageParams.pageLength
+      );
+      setPhotos(resp.sort((a, b) => b.date.localeCompare(a.date)));
+    } catch {
+      showErrorDialog();
+    }
+  };
+
+  useEffect(() => {
+    fetchPhotos();
+  }, [pageParams]);
+
+  const handlePageChange = async (event, value) => {
+    if (pageParams.page === value) {
+      return;
+    }
+    setPageParams((prevState) => ({
+      ...prevState,
+      page: value,
+    }));
+    setPhotos([]);
+  };
+
+  const handlePageLengthChange = (event) => {
+    setPageParams({ page: 1, pageLength: event.target.value });
+  };
+
+  return (
+    <div className="pageContents">
+      <div className="listHeader">
+        <Typography variant="h6" component="div" flexGrow={1}>
+          {localization.heading}
+          <IconButton onClick={showInfoDialog}>
+            <InfoIconOutlined />
+          </IconButton>
+        </Typography>
+        <FormControl
+          sx={{
+            m: 1,
+            minWidth: 120,
+          }}
+        >
+          <Select
+            value={pageParams.pageLength}
+            onChange={handlePageLengthChange}
+            inputProps={{ 'aria-label': 'photos per page' }}
+            variant="standard"
+          >
+            <MenuItem value={1}>1 per page</MenuItem>
+            <MenuItem value={5}>5 per page</MenuItem>
+            <MenuItem value={10}>10 per page</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      <div className="photoList">
+        {photos.length ? (
+          photos.map((photo) => (
+            <PhotoCard
+              key={photo.date}
+              date={photo.date}
+              title={photo.title}
+              explanation={photo.explanation}
+              copyright={photo.copyright}
+              url={photo.url}
+              hdurl={photo.hdurl}
+              mediaType={photo.media_type}
+            />
+          ))
+        ) : (
+          <Loading numPerPage={pageParams.pageLength} />
+        )}
+      </div>
+      <Stack spacing={2}>
+        <Pagination
+          count={99}
+          color="primary"
+          page={pageParams.page}
+          onChange={handlePageChange}
+          sx={{ alignSelf: 'center' }}
+        />
+      </Stack>
+      <InfoDialog
+        title={localization.aboutDialogTitle}
+        text={localization.aboutDialogText}
+        open={infoDialogVisible}
+        onClose={hideInfoDialog}
+      />
+      <InfoDialog
+        title={localization.errorDialogTitle}
+        text={localization.errorDialogText}
+        open={errorDialogVisible}
+        onClose={hideErrorDialog}
+      />
+    </div>
+  );
+}
+
+export default PhotoList;
