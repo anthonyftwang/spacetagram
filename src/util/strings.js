ee4da382c68167bddf15e0751661ffe3f39c2661
Anthony Wang <52114040+anthonyftwang@users.noreply.github.com>
2022-01-09 23:36:00 -0500

    feat: extract user-facing strings

---
+++ src/util/strings.js
@@ -0,0 +1,18 @@
+/**
+ * All user-facing application strings are contained here for flexibility.
+ */
+
+export const en = {
+  appName: 'Spacetagram',
+  heading: 'Recently featured',
+  imgAltTemplate: 'NASA Astronomy Picture of the Day for',
+  aboutDialogTitle: 'About these photos',
+  aboutDialogText:
+    'Discover the cosmos! Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer.\n\nAstronomy Picture of the Day (APOD) is originated, written, coordinated, and edited since 1995 by Robert Nemiroff and Jerry Bonnell. The APOD archive contains the largest collection of annotated astronomical images on the internet.',
+  errorDialogTitle: 'Whoops!',
+  errorDialogText:
+    'There seems to be a problem fetching all those pretty pictures... maybe try again by refreshing the page. 😳',
+  itemsPerPage: 'Items per page',
+};
+
+export default en;
