04940b3c89febdeb8eecf4ae5aa93f3f7ac3d578
Anthony Wang <52114040+anthonyftwang@users.noreply.github.com>
2022-01-11 08:37:10 -0500

    test: photo list length selection

--- src/tests/views/PhotoList.test.js
+++ src/tests/views/PhotoList.test.js
@@ -3,6 +3,8 @@ import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import PhotoList from 'views/PhotoList';
 import localization from 'util/strings';
 
+const itemsPerPageOptions = [1, 5, 10];
+
 it('renders header with list actions', () => {
   window.scrollTo = jest.fn();
   render(<PhotoList />);
@@ -38,6 +40,34 @@ it('shows about dialog when info icon pressed', async () => {
   await waitFor(() =>
     expect(screen.queryByRole('dialog')).not.toBeInTheDocument()
   );
+});
+
+it('expands page length control', () => {
+  window.scrollTo = jest.fn();
+  render(<PhotoList />);
+  const pageLengthControl = screen.getByRole('button', {
+    name: localization.ariaLabels.pageLengthControl,
+  });
+  fireEvent.mouseDown(pageLengthControl);
+  expect(screen.getByRole('listbox')).toBeVisible();
+  itemsPerPageOptions.map((option) =>
+    expect(
+      screen.getByRole('option', {
+        name: `${option} ${localization.itemsPerPage}`,
+      })
+    ).toBeVisible()
+  );
+  fireEvent.click(
+    screen.getByRole('option', {
+      name: `10 ${localization.itemsPerPage}`,
+    })
+  );
+  expect(screen.queryByRole('listbox')).not.toBeInTheDocument();
+  expect(
+    screen.getByRole('button', {
+      name: localization.ariaLabels.pageLengthControl,
+    })
+  ).toHaveTextContent(`10 ${localization.itemsPerPage}`);
 });
 
 it('responds to page change', () => {
