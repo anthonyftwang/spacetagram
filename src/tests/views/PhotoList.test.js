292999f71e61e9437a5aaa0e5df1ff6b6733a6bc
Anthony Wang <52114040+anthonyftwang@users.noreply.github.com>
2022-01-10 21:16:01 -0500

    test: photo list view

---
+++ src/tests/views/PhotoList.test.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import PhotoList from 'views/PhotoList';
+import localization from 'util/strings';
+
+it('renders header with list actions', () => {
+  window.scrollTo = jest.fn();
+  render(<PhotoList />);
+  expect(
+    screen.getByRole('heading', { name: localization.haeding })
+  ).toBeVisible();
+  const aboutButton = screen.getByRole('button', {
+    name: localization.ariaLabels.aboutButton,
+  });
+  expect(aboutButton).toBeVisible();
+  expect(aboutButton).toBeEnabled();
+  const pageLengthControl = screen.getByRole('button', {
+    name: localization.ariaLabels.pageLengthControl,
+  });
+  expect(pageLengthControl).toBeVisible();
+  expect(pageLengthControl).toBeEnabled();
+  expect(pageLengthControl).toHaveTextContent(`5 ${localization.itemsPerPage}`);
+  expect(screen.getByRole('navigation')).toBeVisible();
+});
+
+it('shows about dialog when info icon pressed', async () => {
+  window.scrollTo = jest.fn();
+  render(<PhotoList />);
+  fireEvent.click(
+    screen.getByRole('button', {
+      name: localization.ariaLabels.aboutButton,
+    })
+  );
+  expect(screen.getByRole('dialog')).toBeVisible();
+  fireEvent.click(
+    screen.getByRole('button', { name: localization.dialogClose })
+  );
+  await waitFor(() =>
+    expect(screen.queryByRole('dialog')).not.toBeInTheDocument()
+  );
+});
+
+it('responds to page change', () => {
+  global.scrollTo = jest.fn();
+  render(<PhotoList />);
+  fireEvent.click(
+    screen.getByRole('button', {
+      name: 'Go to page 99',
+    })
+  );
+  expect(
+    screen.getByRole('button', {
+      name: 'page 99',
+    })
+  ).toBeVisible();
+});
+
+it('scrolls to top if current page re-selected', () => {
+  global.scrollTo = jest.fn();
+  render(<PhotoList />);
+  fireEvent.click(
+    screen.getByRole('button', {
+      name: 'page 1',
+    })
+  );
+  expect(global.scrollTo).toHaveBeenCalled();
+});
