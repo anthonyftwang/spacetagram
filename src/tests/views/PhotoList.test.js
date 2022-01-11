7e9987ccf93c1c43a3b4d7b153b9d924fe130776
Anthony Wang <52114040+anthonyftwang@users.noreply.github.com>
2022-01-11 10:03:51 -0500

    test: select about dialog by role and name

--- src/tests/views/PhotoList.test.js
+++ src/tests/views/PhotoList.test.js
@@ -38,7 +38,9 @@ it('shows about dialog when info icon pressed', async () => {
     screen.getByRole('button', { name: localization.dialogClose })
   );
   await waitFor(() =>
-    expect(screen.queryByRole('dialog')).not.toBeInTheDocument()
+    expect(
+      screen.queryByRole('dialog', { name: localization.aboutDialogTitle })
+    ).not.toBeInTheDocument()
   );
 });
 
